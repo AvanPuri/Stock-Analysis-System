1d826f0610f428cca013d1505ebc767fca69bd31
soft-n-5upple <161975148+soft-n-5upple@users.noreply.github.com>
2024-03-27 18:10:24 -0400

    Create SignUp.h

---
+++ SignUp.h
@@ -0,0 +1,25 @@
+#ifndef SignUp_hpp
+#define SignUp_hpp
+
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+class SignUp {
+protected:
+    string username;
+    string password;
+    
+public:
+    SignUp();
+    SignUp(string user, string pass);
+    
+    string Getusername() {return username;}
+    string Getpassword() {return password;}
+    
+    void Setusername(string user) {username = user;}
+    void Setpassword(string pass) {password = pass;}
+    
+    void SaveUser();
+};
+#endif /* SignUp_hpp */
