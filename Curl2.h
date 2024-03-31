42f13c5cc3b091b2cb994fc26c87702f8d350c0e
taranamorim92 <164056925+taranamorim92@users.noreply.github.com>
2024-03-31 00:38:11 -0400

    Create Curl2.h

---
+++ Curl2.h
@@ -0,0 +1,18 @@
+#include <curl/curl.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class NewsCurl
+{
+private:
+    CURL* curl;
+    string stockanalysiswebsite;
+    string link;
+
+public:
+    NewsCurl();
+    ~NewsCurl();
+    string getwebsite(const string& ticker);
+};
